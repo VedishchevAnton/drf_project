af75afd61d106ede2120fe1fc80fa0718dd621d5
Anton Vedischev <vedishchevanton@gmail.com>
2023-07-14 19:04:28 +0300

    Для сериализатора модели курса реализуйте поле вывода уроков.

--- education/serliazers.py
+++ education/serliazers.py
@@ -3,9 +3,20 @@ from rest_framework import serializers
 from education.models import Course, Lesson
 
 
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+
 class CourseSerializer(serializers.ModelSerializer):
     lesson_count = serializers.SerializerMethodField()  # Определение дополнительного поля lesson_count с помощью
+
     # SerializerMethodField(). Это поле будет возвращать количество уроков, связанных с курсом.
+    lessons = LessonSerializer(many=True,
+                               read_only=True)  # Параметр many=True указывает, что поле lessons является коллекцией
+    # объектов, а read_only=True означает, что это поле только для чтения и не будет использоваться для создания или
+    # обновления объектов
 
     @staticmethod
     def get_lesson_count(obj):
@@ -14,10 +25,4 @@ class CourseSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Course
-        fields = ['id', 'title', 'description', 'preview_image', 'lesson_count']
-
-
-class LessonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lesson
         fields = '__all__'
