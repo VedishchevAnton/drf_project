0f173aa2d4ed2c49379b3c5c9590a5106378dd06
Anton Vedischev <vedishchevanton@gmail.com>
2023-07-17 21:22:46 +0300

    реализация работы пользователей со своими курсами и уроками

--- education/permissions.py
+++ education/permissions.py
@@ -1,7 +1,9 @@
 from rest_framework.permissions import BasePermission
 
+from education.models import Course
 
-class CanModify(BasePermission):
+
+class CanModifyCourse(BasePermission):
     def has_permission(self, request, view):
         if request.user.groups.filter(name='Модераторы').exists():
             return True
@@ -10,4 +12,23 @@ class CanModify(BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in ['GET', 'PUT', 'PATCH']:
             return True
+        return False
+
+
+class CanModifyLesson(BasePermission):
+    def has_permission(self, request, view):
+        if request.user.groups.filter(name='Модераторы').exists():
+            return True
+        elif request.method == 'POST':
+            course_id = request.data.get('course')
+            if Course.objects.filter(id=course_id, user=request.user).exists():
+                return True
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in ['GET', 'PUT', 'PATCH']:
+            if request.user.groups.filter(name='Модераторы').exists():
+                return True
+            elif obj.course.user == request.user:
+                return True
         return False
--- education/views.py
+++ education/views.py
@@ -4,42 +4,57 @@ from rest_framework.filters import OrderingFilter
 from rest_framework.permissions import IsAuthenticated
 
 from education.models import Course, Lesson, Payments
-from education.permissions import CanModify
+from education.permissions import CanModifyCourse, CanModifyLesson
 from education.serliazers import CourseSerializer, LessonSerializer, PaymentsSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
-    permission_classes = [IsAuthenticated, CanModify]
+    permission_classes = [IsAuthenticated, CanModifyCourse]
 
 
 class LessonCreateAPIView(generics.CreateAPIView):
     serializer_class = LessonSerializer
-    permission_classes = [IsAuthenticated, CanModify]
+    permission_classes = [IsAuthenticated, CanModifyLesson]
 
 
 class LessonListAPIView(generics.ListAPIView):
     serializer_class = LessonSerializer
-    queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated, CanModify]
+    permission_classes = [IsAuthenticated, CanModifyLesson]
+
+    def get_queryset(self):
+        if self.request.user.groups.filter(name='Модераторы').exists():
+            return Lesson.objects.all()
+        else:
+            return Lesson.objects.filter(course__user=self.request.user)
 
 
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = LessonSerializer
-    queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated, CanModify]
+    permission_classes = [IsAuthenticated, CanModifyLesson]
+
+    def get_queryset(self):
+        if self.request.user.groups.filter(name='Модераторы').exists():
+            return Lesson.objects.all()
+        else:
+            return Lesson.objects.filter(course__user=self.request.user)
 
 
 class LessonUpdateAPIView(generics.UpdateAPIView):
     serializer_class = LessonSerializer
-    queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated, CanModify]
+    permission_classes = [IsAuthenticated, CanModifyLesson]
+
+    def get_queryset(self):
+        if self.request.user.groups.filter(name='Модераторы').exists():
+            return Lesson.objects.all()
+        else:
+            return Lesson.objects.filter(course__user=self.request.user)
 
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated, CanModify]
+    permission_classes = [IsAuthenticated, CanModifyLesson]
 
 
 class PaymentsCreateAPIView(generics.CreateAPIView):
