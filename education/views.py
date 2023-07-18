517e19834b950765c79aa78a7e582b6c1145b446
Anton Vedischev <vedishchevanton@gmail.com>
2023-07-18 08:11:50 +0300

    работа с группой модератор

--- education/views.py
+++ education/views.py
@@ -4,57 +4,57 @@ from rest_framework.filters import OrderingFilter
 from rest_framework.permissions import IsAuthenticated
 
 from education.models import Course, Lesson, Payments
-from education.permissions import CanModifyCourse, CanModifyLesson
+# from education.permissions import CanModifyCourse, CanModifyLesson
 from education.serliazers import CourseSerializer, LessonSerializer, PaymentsSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
-    permission_classes = [IsAuthenticated, CanModifyCourse]
+    permission_classes = [IsAuthenticated]
 
 
 class LessonCreateAPIView(generics.CreateAPIView):
     serializer_class = LessonSerializer
-    permission_classes = [IsAuthenticated, CanModifyLesson]
+    permission_classes = [IsAuthenticated]
 
 
 class LessonListAPIView(generics.ListAPIView):
     serializer_class = LessonSerializer
-    permission_classes = [IsAuthenticated, CanModifyLesson]
+    permission_classes = [IsAuthenticated]
 
-    def get_queryset(self):
-        if self.request.user.groups.filter(name='Модераторы').exists():
-            return Lesson.objects.all()
-        else:
-            return Lesson.objects.filter(course__user=self.request.user)
+    # def get_queryset(self):
+    #     if self.request.user.groups.filter(name='Модераторы').exists():
+    #         return Lesson.objects.all()
+    #     else:
+    #         return Lesson.objects.filter(course__user=self.request.user)
 
 
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = LessonSerializer
-    permission_classes = [IsAuthenticated, CanModifyLesson]
+    permission_classes = [IsAuthenticated]
 
-    def get_queryset(self):
-        if self.request.user.groups.filter(name='Модераторы').exists():
-            return Lesson.objects.all()
-        else:
-            return Lesson.objects.filter(course__user=self.request.user)
+    # def get_queryset(self):
+    #     if self.request.user.groups.filter(name='Модераторы').exists():
+    #         return Lesson.objects.all()
+    #     else:
+    #         return Lesson.objects.filter(course__user=self.request.user)
 
 
 class LessonUpdateAPIView(generics.UpdateAPIView):
     serializer_class = LessonSerializer
-    permission_classes = [IsAuthenticated, CanModifyLesson]
+    permission_classes = [IsAuthenticated]
 
-    def get_queryset(self):
-        if self.request.user.groups.filter(name='Модераторы').exists():
-            return Lesson.objects.all()
-        else:
-            return Lesson.objects.filter(course__user=self.request.user)
+    # def get_queryset(self):
+    #     if self.request.user.groups.filter(name='Модераторы').exists():
+    #         return Lesson.objects.all()
+    #     else:
+    #         return Lesson.objects.filter(course__user=self.request.user)
 
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated, CanModifyLesson]
+    permission_classes = [IsAuthenticated]
 
 
 class PaymentsCreateAPIView(generics.CreateAPIView):
