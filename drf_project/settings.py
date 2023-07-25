b486ef3f412b9e119c11418e9a3ebbeb22de1f22
Anton Vedischev <vedishchevanton@gmail.com>
2023-07-25 16:13:12 +0300

    Настроить CORS(механизм безопасности браузера

--- drf_project/settings.py
+++ drf_project/settings.py
@@ -42,6 +42,7 @@ USER_APPS = [
     'rest_framework_simplejwt',
     'django_filters',
     'drf_yasg',
+    'corsheaders',
 
     'users',
     'education',
@@ -52,6 +53,7 @@ INSTALLED_APPS = STANDARD_APPS + USER_APPS
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -154,3 +156,13 @@ REST_FRAMEWORK = {
 #     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
 #     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 # }
+
+
+CORS_ALLOWED_ORIGINS = [
+    "https://read-only.example.com",
+    "https://read-and-write.example.com",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://read-and-write.example.com",
+]
